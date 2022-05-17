347818a8c8dfdbed3cd163c07b047f8704e5f56e
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-05-17 23:02:00 +0200

    namespaces

---
+++ App/Helpers/Helper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+use App\App;
+use DateTime;
+use App\Models\Teacher;
+use Teacher as Ahmed;
+
+class Helper
+{
+    public function h()
+    {
+        $x = new DateTime();
+        $y = new Teacher();
+        $z = new App();
+        $a = new Ahmed();
+    }
+}
